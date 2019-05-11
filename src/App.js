75b90860fcb0f1d7f9d3f09e9d175fd046910a4d
AlexVillarreal <buffer10gb@gmail.com>
2019-05-11 12:27:29 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { Navbar, Button } from 'react-bootstrap';
+import './App.css';
+
+class App extends Component {
+  goTo(route) {
+    this.props.history.replace(`/${route}`)
+  }
+
+  login() {
+    this.props.auth.login();
+  }
+
+  logout() {
+    this.props.auth.logout();
+  }
+
+  componentDidMount() {
+    const { renewSession } = this.props.auth;
+
+    if (localStorage.getItem('isLoggedIn') === 'true') {
+      renewSession();
+    }
+  }
+
+  render() {
+
+    const { isAuthenticated } = this.props.auth;
+
+    console.log(this);
+
+    
+    return (
+      <div>
+        <Navbar fluid>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">Auth0 - React</a>
+            </Navbar.Brand>
+            <Button
+              bsStyle="primary"
+              className="btn-margin"
+              onClick={this.goTo.bind(this, 'home')}
+            >
+              Home
+            </Button>
+            {
+              !isAuthenticated() && (
+                  <Button
+                    id="qsLoginBtn"
+                    bsStyle="primary"
+                    className="btn-margin"
+                    onClick={this.login.bind(this)}
+                  >
+                    Not authenticated
+                  </Button>
+                )
+            }
+            {
+              isAuthenticated() || (
+                 <Button
+                    id="qsLogoutBtn"
+                    bsStyle="primary"
+                    className="btn-margin"
+                    onClick={this.logout.bind(this)}
+                  >
+                    Authenticated - Log Out
+                  </Button>
+                )
+            }
+          </Navbar.Header>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Auth/auth0-variables.js
@@ -0,0 +1,10 @@
+export const AUTH_CONFIG = {
+  domain: 'dev-lvnwi-78.auth0.com',
+  clientId: 'D2FwOGjk0dPn57UDktSBDv8C3cOdH2Aq',
+  // callbackUrl: 'http://localhost:3000/home' 
+  // callbackUrl: 'http://localhost:3000/callback' 
+  callbackUrl: 'https://kambo.pythonanywhere.com/realStateMaps/' 
+
+  
+}
+                                        
